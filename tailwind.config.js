663b123d0af66c58daa519ba006a68a5d6ef4a84
Alfandi Hasibuan <fandi.premier@gmail.com>
2025-03-03 07:26:30 +0700

    new hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,16 @@ module.exports = {
       fontFamily: {
         poppins: ["Poppins"],
       },
+      animation: {
+        'gradient-x': 'gradient-x 5s ease infinite',
+      },
+      keyframes: {
+        'gradient-x': {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
     },
   },
   plugins: [],
